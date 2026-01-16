8da2de8b677d58d9ab92d15c80c46d2e80300404
F4KHR111 <fakhriwirdiyan05@gmail.com>
2026-01-16 19:36:20 +0700

    perbarui logo aplikasi dan update BASE_URL API pada MedStock

--- app/src/main/java/com/example/medstock/network/RetrofitClient.kt
+++ app/src/main/java/com/example/medstock/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.1.27/medstock_api/"
+    private const val BASE_URL = "http://192.168.1.11/medstock_api/"
 
     val instance: MedStockApiService by lazy {
         Retrofit.Builder()
